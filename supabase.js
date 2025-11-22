f2d2e83b9c5524fdf3a6615bae749528b0c60e97
arsal0299 <shaziahassankhail1@gmail.com>
2025-11-22 14:21:12 +0500

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://ftfqsgvwktmcucxwlzvb.supabase.co",
+  "sb_secret_WO6RS_NiA_LzKLwN41WsXQ_rgREDkLD"
+);
